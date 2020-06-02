43f6b42e7977e3f8bc22938eb01cc59e9b8ab800
Jamie Newbigging <jamienewbigging@gmail.com>
2020-06-02 17:15:32 +0100

    Updates

---
+++ precache-manifest.47a4919e2ad1e2ba92431ebedc16f09e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61972cb29b03ad8e8d9662b03a4e9709",
+    "url": "/quote-machine/index.html"
+  },
+  {
+    "revision": "4d318bfc51ed6e09cbf2",
+    "url": "/quote-machine/static/css/main.72bd3fa5.chunk.css"
+  },
+  {
+    "revision": "67e2dfb7a2986c8769dc",
+    "url": "/quote-machine/static/js/2.36631971.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quote-machine/static/js/2.36631971.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d318bfc51ed6e09cbf2",
+    "url": "/quote-machine/static/js/main.590396a5.chunk.js"
+  },
+  {
+    "revision": "cbc9f74f0ef17ee9ad82",
+    "url": "/quote-machine/static/js/runtime-main.6752162c.js"
+  }
+]);
